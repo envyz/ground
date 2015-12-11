9dd47d3e54ec960db0fb8bee554fd1e9444fde91
정소영 <envyz710@corp.auction.co.kr>
2015-12-11 16:03:12 +0900

    jquery ajax post test

--- web/app.js
+++ web/app.js
@@ -18,8 +18,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'data')));
 
 app.use('/', routes);
 
